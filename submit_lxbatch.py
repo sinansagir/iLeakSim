7b2e1fc07aa8f305f62cebebc0efa7f6bf8474e0
Sinan <Sinan_Sagir@brown.edu>
2016-03-01 20:53:38 +0100

    fixes

--- submit_lxbatch.py
+++ submit_lxbatch.py
@@ -2,29 +2,29 @@
 
 import os,datetime
 
-cmsswDir=os.environ['CMSSW_BASE']+'/src'
+cmsswDir  =os.environ['CMSSW_BASE']+'/src'
 scriptDir ='iLeakSim'
 scriptName='iLeakSim.py'
-queue = "1nh"
+queue = "1nd" # give bsub queue -- 8nm (8 minutes), 1nh (1 hour), 8nh, 1nd (1day), 2nd, 1nw (1 week), 2nw 
 
 cTime=datetime.datetime.now()
 date='%i_%i_%i'%(cTime.year,cTime.month,cTime.day)
 time='%i_%i_%i'%(cTime.hour,cTime.minute,cTime.second)
 pfix=''
 
-os.mkdir('condor_'+date+pfix)
 outDir=cmsswDir+'/'+scriptDir+'/condor_'+pfix+date#+'_'+time
+os.mkdir(outDir)
 
-NtotModules = 5#13196 #Nentries in dPdT.root
-NmodulePerJob = 1#30
+NtotModules = 13196 #Nentries in dPdT.root
+NmodulePerJob = 30
 
 countJobs=0
 for it in range(0,NtotModules/NmodulePerJob+1):
-	##### creates directory and file list for job #######
+	#create job directory
 	os.system("mkdir "+outDir+"/"+str(it))
 	os.chdir(outDir+"/"+str(it))
 	outfile='entry_'+str(it)+'.root'
-	##### creates jobs #######
+	#create jobs
 	with open('job.sh', 'w') as fout:
 		fout.write("#!/bin/sh\n\n\n")
 		fout.write("echo 'START---------------'\n")
@@ -35,11 +35,9 @@ for it in range(0,NtotModules/NmodulePerJob+1):
 		fout.write("cp -r "+cmsswDir+"/"+scriptDir+"/* ./\n")
 		fout.write("python "+cmsswDir+"/"+scriptDir+"/"+scriptName+" "+str(it)+" "+str(NmodulePerJob)+" "+outfile+"\n")
 		fout.write("rfcp *.root "+outDir+"/"+"\n")
-		fout.write("echo 'STOP---------------'\n")
-		fout.write("echo\n")
-		fout.write("echo\n")
+		fout.write("echo 'STOP---------------'\n\n")
 	os.system("chmod 755 job.sh")
-	###### sends bjobs ######
+	#submit bjobs
 	os.system("bsub -q "+queue+" -o logs job.sh")
 	print "job nr " + str(it) + " submitted"
 	os.chdir("../..")
